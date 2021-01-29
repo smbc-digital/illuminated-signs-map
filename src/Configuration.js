56be78e9738b6a5704804aa343135fe4e3f73549
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-01-29 16:38:43 +0000

    First commit

---
+++ src/Configuration.js
@@ -0,0 +1,93 @@
+import Leaflet from 'leaflet'
+import { illuminatedsignPopup} from './Popups' //devsitesPopup, notdevsitesPopup}
+import { illuminatedsignStyle} from './Styles'
+
+const Configuration = {
+    Map: {
+        StartingLatLng: [53.391067,-2.1197936],
+        StartingZoom: 2,
+        FullscreenControl: true,
+        DisplayLayerControls: true,
+        DisplayGrayScale: true,
+        DisplayStreets: true,
+        EnableAddressSearch: true,
+        EnableLocateControl: true
+    },
+    DynamicData: 
+    [
+        //{
+          //  key: 'Tree Preservation Orders',
+            //url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=trees:tpo_merged&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            //layerOptions: {
+            //    onEachFeature: tpoPopup,
+            //    maxZoom: 2,
+            //    style: tpoStyle
+            //},
+            //displayOverlay: true,
+            //visibleByDefault: true
+        //},
+
+        {
+            key: 'Illuminated Signs',
+            url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=highway_assets:illuminated_sign_reporting&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
+            layerOptions: {
+                maxZoom: 16,
+                onEachFeature: illuminatedsignPopup,
+                pointToLayer: (feature, latlng) => {
+                    return Leaflet.circleMarker (latlng, illuminatedsignStyle (feature))
+
+                }
+            },
+            displayOverlay: false,
+            visibleByDefault: true
+        },
+        
+        {
+            key: 'os1250_line',
+            url: 'http://spatial.stockport.gov.uk/geoserver/wms?',
+            layerOptions: {
+                maxZoom: 20,
+                minZoom: 18,
+                layers: 'base_maps:os1250_line',
+                format: 'image/png',
+                transparent: true
+            },
+            displayOverlay: false,
+            visibleByDefault: true
+        },
+        {
+            key: 'os1250_text',
+            url: 'http://spatial.stockport.gov.uk/geoserver/wms?',
+            layerOptions: {
+                maxZoom: 20,
+                minZoom: 18,
+                layers: 'base_maps:os1250_text',
+                format: 'image/png',
+                transparent: true
+            },
+            displayOverlay: false,
+            visibleByDefault: true
+        }
+        
+    ],
+    StaticData: 
+    [
+        {
+            key: 'boundary',
+            url: 'https://spatialgeojson.s3-eu-west-1.amazonaws.com/webmapping/boundary.geojson',
+            layerOptions: {
+                interactive: false,
+                maxZoom: 9,
+                style: {
+                    color: '#000',
+                    weight: 4,
+                    opacity: 1,
+                    fillColor: '#000',
+                    fillOpacity: 0
+                }
+            }
+        }
+    ]
+}
+
+export default Configuration
---
+++ src/Popups/index.js
@@ -0,0 +1,65 @@
+import { getTargetUrl } from '../Helpers'
+
+const signActivePopup = feature => {
+  const varName = getTargetUrl()
+
+  return `<div class="item"><i class="tag fa fa-map-marker"></i><p class="title">Location </p><p class="info">${feature.properties.location_description}</p></div><hr/>
+  <div class="item"><i class="tag fa fa-tag"></i><p class="title">Sign ID </p><p class="info">${feature.properties.feature_id}</p></div>
+  <a class="button-primary" href="${varName}/lit-sign-bollard/report-an-issue/lit-sign/fault-type?assetId=${feature.properties.central_asset_id}&siteCode=${feature.properties.site_code}">Report this sign</a>`
+}
+
+const signFaultPopup = feature => {
+ const varName = getTargetUrl()
+ let noOfDays = Math.floor(
+   (new Date() - new Date(feature.properties.job_entry_date)) /
+     (1000 * 3600 * 24)
+ )
+ let lastUpdated = Math.floor(
+   (new Date() - new Date(feature.properties.logged_date)) / (1000 * 3600 * 24)
+ )
+ const defaultMessage = noOfDays
+   ? `A fault with this sign was reported ${noOfDays} days ago`
+   : 'A fault with this sign was reported'
+ const showLastUpdated = lastUpdated
+   ? `<div class="last-updated">Last updated ${lastUpdated} days ago</div>`
+   : ''
+
+ return `<div class="item"><i class="tag fa fa-map-marker"></i><p class="title">Location </p><p class="info">${feature.properties.location_description}</p></div><hr>
+    <div class="item"><i class="tag fa fa-tag"></i><p class="title">Sign ID </p><p class="info">${feature.properties.feature_id}</p></div>
+    <div class= "message-fault">${defaultMessage}</div>
+    <a class="button-primary" href="${varName}/track-a-report/details/${feature.properties.ext_system_ref}">View reported fault</a>
+    ${showLastUpdated}`
+
+}
+
+const signMaintenancePopup = feature => {
+ const message =
+   feature.properties.message ??
+   'This sign is part of a maintenance programme and will be fixed without a need to report'
+
+ return`<div class="item"><span class="iconify" data-icon="fa-map-marker" data-inline="false"></span></i><p class="title">Location </p><p class="info">${feature.properties.location_description}</p></div><hr>
+    <div class="item"><i class="tag fa fa-tag"></i><p class="title">Sign ID </p><p class="info">${feature.properties.feature_id}</p></div>
+    <div class= "message-maintenance">${message}</div>`
+
+}
+
+const illuminatedsignPopup = (feature, layer) => {
+  var content = getcontent_sign(feature)
+
+  layer.bindPopup(content)
+}
+
+const getcontent_sign = feature => {
+  switch  (feature.properties.raise_new_job) {  
+    case 1:
+        return signActivePopup(feature)
+    case 2:
+        return signMaintenancePopup(feature)
+    case 3:
+        return signFaultPopup(feature)    
+  }
+}
+
+export {
+  illuminatedsignPopup 
+}
